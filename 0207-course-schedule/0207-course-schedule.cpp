5a2b679a7a0c201c9d4ee382945d8db8ca748123
Lipsa Behera <78685542+lipsa22@users.noreply.github.com>
2023-07-13 22:59:55 +0530

    Time: 15 ms (98.49%), Space: 13.2 MB (83.94%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> gr(numCourses);
+vector<int> indegrees(numCourses, 0);
+for (const auto& prerequisite : prerequisites) {
+int course = prerequisite[0];
+int pre_course = prerequisite[1];
+gr[pre_course].push_back(course);
+indegrees[course]++;
+}
+queue<int> q;
+for (int course = 0; course < numCourses; ++course) {
+if (indegrees[course] == 0) {
+q.push(course);
+}
+}
+while (!q.empty()) {
+int course = q.front();
+q.pop();
+for (int next_course : gr[course]) {
+indegrees[next_course]--;
+if (indegrees[next_course] == 0) {
+q.push(next_course);
+}
+}
+}
+for (int indegree : indegrees) {
+if (indegree != 0) {
+return false;
+}
+}
+return true;
+        
+    }
+};
