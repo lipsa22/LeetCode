d97bf3255e9e1409b8cd5ac2c5d2d631fe42533d
Lipsa Behera <78685542+lipsa22@users.noreply.github.com>
2023-07-25 23:48:40 +0530

    Added solution - LeetHub

---
+++ Level order traversal in spiral form - GFG/level-order-traversal-in-spiral-form.cpp
@@ -0,0 +1,170 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+vector<int> findSpiral(Node *root);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+int main() {
+    int t;
+    string  tc;
+    getline(cin,tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+
+        vector<int> vec = findSpiral(root);
+        for(int x : vec)
+        cout<<x<<" ";
+        cout << endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
+
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child  
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+
+//Function to return a list containing the level order traversal in spiral form.
+void reverse(vector<int>& list, int st, int end)
+{
+    while (st < end)
+    {
+        int temp = list[st];
+        list[st] = list[end];
+        list[end] = temp;
+        st++;
+        end--;
+    }
+}
+vector<int> findSpiral(Node *root)
+{
+    //Your code here
+     vector<int> ans;
+    queue<Node*> q;
+    int level = 0;
+    q.push(root);
+    while (!q.empty())
+    {
+        int size = q.size();
+        int st = ans.size();
+        int end = ans.size() + size - 1;
+        for (int i = 0; i < size; i++)
+        {
+            Node* nn = q.front();
+            q.pop();
+            ans.push_back(nn->data);
+            if (nn->left)
+                q.push(nn->left);
+            if (nn->right)
+                q.push(nn->right);
+        }
+        if (level % 2 == 0)
+        {
+            reverse(ans, st, end);
+        }
+        level++;
+    }
+    return ans;
+}
