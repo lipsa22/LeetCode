80e0650e2c38d6adcce11e76ae78706d94c1bb8e
Lipsa Behera <78685542+lipsa22@users.noreply.github.com>
2023-08-30 23:25:46 +0530

    Time: 2 ms (42.93%), Space: 6.1 MB (41.18%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+         vector<int>a(n+1,0);
+        a[0] = 1;
+        a[1] = 1;
+        for(int i = 2 ; i <= n ; i++) {
+            a[i] += a[i-1];
+            a[i] += a[i-2];
+        }
+        return a[n];
+        
+    }
+};
